0e4bd2e0b0bc9f9b172c741bff856f958877c3a4
Sumit Verma <139337175+Sharshit24@users.noreply.github.com>
2024-12-27 21:23:08 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=1143
+let accountEmail="sumitverma@google.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+
+console.log(accountId); 
+accountEmail="sv@sv.com"
+accountPassword="1444"
+accountCity="Delhi"
+
+console.table([accountEmail,accountId,accountPassword,accountCity]);
