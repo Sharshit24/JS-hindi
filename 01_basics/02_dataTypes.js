94486dd3b4284482705cefdfb675b901c7b8b290
Sumit Verma <139337175+Sharshit24@users.noreply.github.com>
2024-12-27 21:23:23 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treal all js code as newer version
+
+// alert (3+3) //we are using node js, not broswer
+
+// Data Types
+
+
+//number
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol
+
+
+//object
+
+console.log(typeof 18)
