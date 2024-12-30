61bf1f1c428bda276c15a6d408eafec98021829a
Sumit Verma <139337175+Sharshit24@users.noreply.github.com>
2024-12-30 17:48:03 +0000

    conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,13 +1,34 @@
-let score="83aa"
-console.log(score)
+// let score="83aa" 
+// console.log(score)
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 
-let valueInNumber=Number(score);
-console.log(valueInNumber);
+// let valueInNumber=Number(score);
+// console.log(valueInNumber);
 
-let isLoggedIn=1
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+
+
+// let someNumber =33
+
+// let stringNumber =String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// operations
+
+let value =3
+let negValue=-value
+// console.log(negValue);
+
+let str1="hello"
+let str2=" sumit"
+let str3=str1+str2;
+
+console.log(str3)
