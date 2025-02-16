5314e595ed2a391d17c50c30ab5cc6a8733199ee
Sumit Verma <139337175+Sharshit24@users.noreply.github.com>
2025-02-16 18:15:27 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,16 @@
+// var c=3000
+
+let a=300
+
+if (true){
+    let a=10
+    const b=20
+    // var c=30
+}
+
+console.log(a);
+// console.log(b);
+
+for(let i=0;i<Array.length;i++){
+    Array[i]
+}
