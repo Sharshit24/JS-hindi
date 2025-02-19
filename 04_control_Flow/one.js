6c8a7ebfb1d69b18b01994959ec0c001f6ed72a5
Sumit Verma <139337175+Sharshit24@users.noreply.github.com>
2025-02-19 23:18:48 +0000

    if else

---
+++ 04_control_Flow/one.js
@@ -0,0 +1,18 @@
+//if
+
+// const temperature =41
+// if (temperature===41) {
+//     console.log("less than 50");
+// }else{
+//     console.log("greater than 50");
+// }
+
+// const score =200
+
+// if(score>100){
+//     const power="fly"
+//     console.log(`use power:${power}`);   
+// }
+
+const balance =1000
+if(balance>500) console.log("test");
